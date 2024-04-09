40e22d81515e0a36203ec80c662f1535b24b6b12
Nathanage3 <sisay954@gmail.com>
2024-04-09 15:36:07 +0300

    condensed arrow function

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,14 +1,7 @@
 export default getSumOfHoods = (
   initialNumber,
-  expansion1989,
-  expansion2019
+  expansion1989 = 89,
+  expansion2019 = 19
 ) => {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
   return initialNumber + expansion1989 + expansion2019;
 };
