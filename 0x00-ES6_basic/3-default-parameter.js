b5eb77f419963b15ab26f5bc3c07733041612697
Nathanage3 <sisay954@gmail.com>
2024-04-09 15:37:19 +0300

    condensed arrow function

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,7 @@
-export default getSumOfHoods = (
+export default function getSumOfHoods(
   initialNumber,
   expansion1989 = 89,
   expansion2019 = 19
-) => {
+) {
   return initialNumber + expansion1989 + expansion2019;
-};
+}
